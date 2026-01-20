8d9b9b530ace8de8665578a582a67db72fecaac8
User <user@example.com>
2026-01-20 21:52:20 +0500

    Update base path to meta_it_school

--- Rustamoff/src/main.jsx
+++ Rustamoff/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Rustamoff/">
+    <BrowserRouter basename="/meta_it_school/">
       <App />
     </BrowserRouter>
   </StrictMode>,
--- Rustamoff/vite.config.js
+++ Rustamoff/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Rustamoff/',
+  base: '/meta_it_school/',
   plugins: [react(), tailwindcss(),],
 })
